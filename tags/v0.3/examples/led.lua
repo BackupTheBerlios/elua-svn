20361ada2578f0a3ba2606b2517109c388f2decd
bogdanm <bogdanm@67d3b963-1d53-0410-ba23-e466f4efc467>
2008-08-09 11:20:24 +0000

    Official verson 0.3

---
+++ tags/v0.3/examples/led.lua
@@ -0,0 +1,28 @@
+-- eLua test 
+
+if pd.platform() == "AT91SAM7X" then
+  ledpin = pio.PB_20
+elseif pd.platform() == "LM3S" then
+  ledpin = pio.PF_0
+elseif pd.platform() == "STR9" then
+  ledpin = pio.P9_0
+else
+  print( "Unknown platform " .. pd.platform() )
+  return
+end
+
+function cycle()
+  pio.set( ledpin )
+  tmr.delay( tmr.TMR0, 500000 )
+  pio.clear( ledpin )
+  tmr.delay( tmr.TMR0, 500000 )
+end
+
+pio.output( ledpin )
+print( "Hello from eLua on " .. pd.cpu() )
+print "Watch your LED blinking :)"
+print "Press any key to end this demo.\n"
+
+while uart.recv( uart.UART0, 0, 0 ) < 0 do
+  cycle()
+end
---
+++ tags/v0.3/src/main.c
@@ -0,0 +1,193 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "type.h"
+#include "devman.h"
+#include "platform.h"
+#include "romfs.h"
+#include "xmodem.h"
+#include "shell.h"
+#include "build.h"
+#include "lua.h"
+#include "term.h"
+
+extern char etext[];
+extern char end[];
+
+// ****************************************************************************
+// XMODEM support code
+
+// Maximum file size that can be received via XMODEM
+// Should be a multiple of 128
+#define XMODEM_MAX_FILE_SIZE    4096
+
+#ifdef BUILD_XMODEM
+
+#define XMODEM_UART_ID          0
+#define XMODEM_TIMER_ID         0
+
+static void xmodem_send( u8 data )
+{
+  platform_uart_send( XMODEM_UART_ID, data );
+}
+
+static int xmodem_recv( u32 timeout )
+{
+  return platform_uart_recv( XMODEM_UART_ID, XMODEM_TIMER_ID, timeout );
+}
+
+#else // #ifdef BUILD_XMODEM
+
+static void xmodem_send( u8 data )
+{
+  data = data;
+}
+
+static int xmodem_recv( u32 timeout )
+{
+  timeout = timeout;
+  return -1;
+}
+
+#endif // #ifdef BUILD_XMODEM
+
+// ****************************************************************************
+// Terminal support code
+
+#define TERMINAL_LINES        25
+#define TERMINAL_COLS         80
+
+#ifdef BUILD_TERM
+
+#define TERM_UART_ID          0
+#define TERM_TIMER_ID         0
+#define TERM_TIMEOUT          100000
+
+static void term_out( u8 data )
+{
+  platform_uart_send( TERM_UART_ID, data );
+}
+
+static int term_in( int mode )
+{
+  if( mode == TERM_INPUT_DONT_WAIT )
+    return platform_uart_recv( TERM_UART_ID, TERM_TIMER_ID, 0 );
+  else
+    return platform_uart_recv( TERM_UART_ID, TERM_TIMER_ID, PLATFORM_UART_INFINITE_TIMEOUT );
+}
+
+static int term_translate( u8 data )
+{
+  int c;
+  
+  if( isprint( data ) )
+    return data;
+  else if( data == 0x1B ) // escape sequence
+  {
+    // If we don't get a second char, we got a simple "ESC", so return KC_ESC
+    // If we get a second char it must be '[', the next one is relevant for us
+    if( platform_uart_recv( TERM_UART_ID, TERM_TIMER_ID, TERM_TIMEOUT ) == -1 )
+      return KC_ESC;
+    if( ( c = platform_uart_recv( TERM_UART_ID, TERM_TIMER_ID, TERM_TIMEOUT ) ) == -1 )
+      return KC_UNKNOWN;
+    switch( c )
+    {
+      case 0x41:
+        return KC_UP;
+      case 0x42:
+        return KC_DOWN;
+      case 0x43:
+        return KC_RIGHT;
+      case 0x44:
+        return KC_LEFT;               
+    }
+  }
+  else if( data == 0x0D )
+  {
+    // CR/LF sequence, read the second char (LF) if applicable
+    platform_uart_recv( TERM_UART_ID, TERM_TIMER_ID, TERM_TIMEOUT );
+    return KC_ENTER;
+  }
+  else
+  {
+    switch( data )
+    {
+      case 0x09:
+        return KC_TAB;
+      case 0x16:
+        return KC_PAGEDOWN;
+      case 0x15:
+        return KC_PAGEUP;
+      case 0x05:
+        return KC_END;
+      case 0x01:
+        return KC_HOME;
+      case 0x7F:
+      case 0x08:
+        return KC_BACKSPACE;
+    }
+  }
+  return KC_UNKNOWN;
+}
+
+#else // #ifdef BUILD_TERM
+
+static void term_out( u8 data )
+{
+  data = data;
+}
+
+static int term_in( int mode )
+{
+  mode = mode;
+  return -1;
+}
+
+static int term_translate( u8 data )
+{
+  data = data;
+  return -1;
+}
+
+#endif // #ifdef BUILD_TERM
+
+// ****************************************************************************
+//  Program entry point
+
+int main( void )
+{
+  // Initialize platform first
+  if( platform_init() != PLATFORM_OK )
+  {
+    // This should never happen
+    while( 1 );
+  }
+    
+  // Initialize device manager
+  dm_init();
+  
+  // Register the ROM filesystem
+  dm_register( romfs_init() );  
+  
+  // Initialize XMODEM
+  xmodem_init( xmodem_send, xmodem_recv );    
+  
+  // Initialize terminal
+  term_init( TERMINAL_LINES, TERMINAL_COLS, term_out, term_in, term_translate );
+  
+  printf( ".text ends at %p, first free RAM is at %p, last free ram is at %p\r\n", etext, platform_get_first_free_ram(), platform_get_last_free_ram() );
+  
+  // Run the shell
+  if( shell_init( XMODEM_MAX_FILE_SIZE ) == 0 )
+  {
+    printf( "Unable to initialize the eLua shell!\n" );
+    // Start Lua directly
+    char* lua_argv[] = { "lua", NULL };
+    lua_main( 1, lua_argv );
+  }
+  else
+    shell_start();
+
+  while( 1 );
+}
